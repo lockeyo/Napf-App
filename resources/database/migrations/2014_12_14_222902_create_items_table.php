3e2fd8cb39a94c4245fee504ecceb35cbd4a1e9f
lockeyo <Men@NKA-WN-MEN.ptvag.ptv.de>
2014-12-15 01:33:59 +0100

    Show Images

--- resources/database/migrations/2014_12_14_222902_create_items_table.php
+++ resources/database/migrations/2014_12_14_222902_create_items_table.php
@@ -17,6 +17,7 @@ class CreateItemsTable extends Migration {
 			$table->increments('id');
 			$table->string('name');
 			$table->string('price');
+			$table->string('image');
 			$table->boolean('available');
 			$table->timestamps();
 		});
